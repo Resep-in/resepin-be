af29dc3cb5397cfc57d5695cab0c4dea6e17689b
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2025-07-22 23:41:47 +0700

    refactor: docker compose command^Ceployment setups

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 cp .env.example .env
-docker-compose build
+docker compose build
 
 docker run --rm -it --volume $(pwd):/app resepin-be composer install --no-dev --optimize-autoloader
 docker run --rm -it --volume $(pwd):/app resepin-be php artisan key:generate
 
 # up composer
-docker-compose up -d
-docker-compose exec api php artisan migrate
-docker-compose ps
+docker compose up -d
+docker compose exec api php artisan migrate
+docker compose ps
