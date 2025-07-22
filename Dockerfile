7d6ac5e4b3d438bced2b3cc37dc7cd6aaf38272d
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2025-07-22 23:56:49 +0700

    refactor: docker php extension for deployment setups

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN apt-get update && apt-get install -y \
 
 # Clear cache
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
-RUN docker-php-ext-install pdo_mysql exif pcntl bcmath gd xml iconv dom simplexml xmlreader intl soap zip
+RUN docker-php-ext-install pdo_pgsql zip
 RUN docker-php-ext-configure intl
 
 # Configure PHP
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ docker compose build
 docker run --rm -it --volume $(pwd):/app resepin-be composer install --no-dev --optimize-autoloader
 docker run --rm -it --volume $(pwd):/app resepin-be php artisan key:generate
 
+docker run --rm -it --volume $(pwd):/app resepin-be php artisan octane:install --server=frankenphp
+
 # up composer
 docker compose up -d
 docker compose exec api php artisan migrate
